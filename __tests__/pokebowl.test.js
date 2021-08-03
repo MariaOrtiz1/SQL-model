dc5391cd8d05cc9fac891a9be18895f29530b29b
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-08-03 11:44:42 -0700

    Added pokebowl static post route, test is passing with hard coded answer

--- __tests__/pokebowl.test.js
+++ __tests__/pokebowl.test.js
@@ -2,7 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-import Pokebowl from '../lib/models/Pokebowl.js';
+// import Pokebowl from '../lib/models/Pokebowl.js';
 
 describe('pokebowl routes', () => {
   beforeEach(() => {
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,10 +1,9 @@
 DROP TABLE IF EXISTS pokebowl;
-
 CREATE TABLE pokebowl (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     base TEXT NOT NULL,
     protein_choice TEXT NOT NULL,
     protein_addition TEXT,
     toppings TEXT,
-    sauce TEXT,
-);
+    sauce TEXT
+)
