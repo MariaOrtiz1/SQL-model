e19c701110efb9ed6bc5ee41d7fcb00f309baf39
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-08-03 10:52:37 -0700

    Added drop table, controllers and models files, and wrote the first test. Added pokebowl POST test, test is failing

---
+++ __tests__/pokebowl.test.js
@@ -0,0 +1,23 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+import Pokebowl from '../lib/models/Pokebowl.js';
+
+describe('pokebowl routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('creates an order for a pokebowl via POST', async () => {
+    const pokebowl = { base: 'white sushi rice', protein: 'salmon', proteinAddition: 'tofu', toppings: '', sauce: 'poke sauce' };
+    const res = await request(app)
+      .post('/api/v1/pokebowls')
+      .send(pokebowl);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...pokebowl
+    });
+  });
+});
---
+++ sql/setup.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS pokebowl;
+
+CREATE TABLE pokebowl (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    base TEXT NOT NULL,
+    protein_choice TEXT NOT NULL,
+    protein_addition TEXT,
+    toppings TEXT,
+    sauce TEXT,
+);
